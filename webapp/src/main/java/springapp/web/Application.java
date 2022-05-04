6e966ef9df0fa8312534cf261a5b0f73a37839eb
jaunn-victorr <joao.vitor.ptal@gmail.com>
2022-05-04 17:20:22 +0000

    springapp-no-file

---
+++ webapp/src/main/java/springapp/web/Application.java
@@ -0,0 +1,11 @@
+package.springapp.web;
+
+import org.springfamework.boot.SpringApplication;
+import org.springframework.boot.autoconfigureSpringBootAplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringAPplication.run(Application.class, args);
+    }
+}
